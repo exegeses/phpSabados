43a248451f523a160e41370de72081db2f35be44
Marcos Pinardi <exegeses@gmail.com>
2020-03-07 11:05:25 -0300

    tarea funciones caterçgorias

--- sistema/funciones/categorias.php
+++ sistema/funciones/categorias.php
@@ -3,6 +3,15 @@
     ###################################
     ########## CRUD DE CATEGORIAS #########
 
+    function listarCategorias()
+    {
+        $link = conectar();
+        $sql = "SELECT idCategoria, catNombre
+                   FROM categorias";
+        $resultado = mysqli_query( $link, $sql );
+        return $resultado;
+    }
+
     /**
      * listarCategorias()
      * verCategoriaPorID()
